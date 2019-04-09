d8eebc7e0ee174fb097b53b8c2ed09b1ae898b8c
victors <victoraugusto101093@gmail.com>
2019-04-09 17:05:23 -0300

    Criação das classes commit

---
+++ Entities/Comment.cs
@@ -0,0 +1,20 @@
+namespace PushEx
+{
+    class Comment
+    {
+        public string Text { get; set; }
+
+        public Comment()
+        {
+        }
+
+        public Comment(string text)
+        {
+            Text = text;
+        } 
+
+
+    }
+
+}
+
---
+++ Entities/Post.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace PushEx
+{
+    class Post
+    {
+        public DateTime Moment { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int Likes { get; set; }
+        public List <Comment> Coments { get; set; } = new List<Comment>();
+
+        public Post()
+        {
+        }
+
+        public Post(DateTime moment, string title, string content, int likes)
+        {
+            Moment = moment;
+            Title = title;
+            Content = content;
+            Likes = likes;
+        }
+    }
+}
